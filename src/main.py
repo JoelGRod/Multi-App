a09c873bd1e7f9bc0fea33dc24a122d1760ccb27
JoelGRod <joelgrod@gmail.com>
2022-04-21 00:28:07 +0100

    first commit

---
+++ src/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import view
+
+def main():
+    print("Hello World")
+    view()
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/view.py
@@ -0,0 +1,11 @@
+# Main view here
+from tkinter import *
+
+root = Tk()
+root.title('Multi App')
+root.geometry('500x500')
+
+label = Label(root, text='Hello World')
+label.pack()
+
+root.mainloop()
