58c75549f96acced80b1c4056b313b637ffc5497
joelgrod <joelgrod@gmail.com>
2022-04-27 19:34:23 +0100

    youtube downloader

--- src/yt_downloader/main/app/pages/yt_downloader_page.py
+++ src/yt_downloader/main/app/pages/yt_downloader_page.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
 
 import apafy
 
@@ -8,15 +10,18 @@ LARGEFONT =("Verdana", 35)
 class YoutubeDownloaderHome(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.download_folder = tk.StringVar()
-        self.download_folder.set("No Specified")
+        self.download_folder = tk.StringVar(value = "./")
         self.video_url = tk.StringVar()
-        self.video_url.set("No Specified")
-
-        # UI
-        title_frame = tk.Frame(self, padx = 10, pady = 10, width=100)
+        self.video = None
+        self.progressbar = None
+    
+        self.create_template()
+    
+    # Template
+    def create_template(self):
+        title_frame = tk.Frame(self, padx = 10, pady = 10)
         title_frame.grid(row = 0, column = 0, sticky = "w")
-        dl_frame = tk.Frame(self, padx = 10, pady = 10, width=100)
+        dl_frame = tk.Frame(self, padx = 10, pady = 10)
         dl_frame.grid(row = 1, column = 0, sticky = "w")
 
         # Title frame
@@ -24,84 +29,95 @@ class YoutubeDownloaderHome(tk.Frame):
         label.grid(row = 0, column = 0)
 
         # Download Frame
-        url_label = tk.Label(dl_frame, text="Video URL:")
-        url_label.grid(row = 0, column = 0, padx = 10, sticky = "w")
-        url_entry = tk.Entry(dl_frame, width = 40)
-        url_entry.grid(row = 0, column = 1, padx = 10)
-        set_video_url_button = tk.Button(
-            dl_frame, text="Set Video URL", 
-            command = lambda: self.save_video_url(url_entry))
-        set_video_url_button.grid(row = 0, column = 2, padx = 10, sticky = "w")
-
-        folder_label = tk.Label(dl_frame, text="Download Folder:")
-        download_folder_label = tk.Label(dl_frame, textvariable=self.download_folder)
-        folder_label.grid(row = 1, column = 0, padx = 10, sticky = "w")
-        download_folder_label.grid(row = 1, column = 1, padx = 10, sticky = "w")
-        select_folder_button = tk.Button(
-            dl_frame, text="Download Folder", 
+        # Setting Download Folder
+        download_folder_label = tk.Label(dl_frame, textvariable = self.download_folder)
+        download_folder_label.grid(row = 0, column = 0, padx = 10, sticky = "w")
+        set_download_folder_button = tk.Button(
+            dl_frame, text = "Set Download Folder", 
             command = self.save_download_folder)
-        select_folder_button.grid(row = 1, column = 2, padx = 10, sticky = "w")
+        set_download_folder_button.grid(row = 0, column = 1, padx = 10, sticky = "w")
 
-        url_video_label = tk.Label(dl_frame, text="Video URL:")
-        url_video_confirmed_label = tk.Label(dl_frame, textvariable=self.video_url)
-        url_video_label.grid(row = 2, column = 0, padx = 10, sticky = "w")
-        url_video_confirmed_label.grid(row = 2, column = 1, padx = 10, sticky = "w")
+        # Setting YouTube Video URL
+        video_url_entry = tk.Entry(dl_frame, width = 60)
+        video_url_entry.grid(row = 1, column = 0, padx = 10, pady = 10, sticky = "w")
+        set_video_url_button = tk.Button(
+            dl_frame, text = "Set Video URL", 
+            command = lambda: self.save_video_url(video_url_entry))
+        set_video_url_button.grid(row = 1, column = 1, padx = 10, sticky = "w")
 
+    def show_video_info(self):
+        info_frame = tk.Frame(self, padx = 10, pady = 10)
+        info_frame.grid(row = 2, column = 0, sticky = "w")
+
+        author_label = tk.Label(info_frame, text = "Author:")
+        author_label.grid(row = 0, column = 0, padx = 10, sticky = "w")
+        author = tk.Label(info_frame, text = self.video.author)
+        author.grid(row = 0, column = 1, padx = 10, sticky = "w")
+
+        title_label = tk.Label(info_frame, text = "Title:")
+        title_label.grid(row = 1, column = 0, padx = 10, sticky = "w")
+        title = tk.Label(info_frame, text = self.video.title)
+        title.grid(row = 1, column = 1, padx = 10, sticky = "w")
+
+        video_url_label = tk.Label(info_frame, text = "Url")
+        video_url_label.grid(row = 2, column = 0, padx = 10, sticky = "w")
+        video_url = tk.Label(info_frame, textvariable = self.video_url)
+        video_url.grid(row = 2, column = 1, padx = 10, sticky = "w")
     
+    def show_download_buttons(self):
+        buttons_frame = tk.Frame(self, padx = 10, pady = 10)
+        buttons_frame.grid(row = 3, column = 0, sticky = "w")
+        # Download Resource
         download_video_button = tk.Button(
-            dl_frame, text="Get Video", 
+            buttons_frame, text="Get Video", 
             command = lambda: self.download("video"))
-        download_video_button.grid(row = 3, column = 0, padx = 10, sticky = "w")
+        download_video_button.grid(row = 0, column = 0, padx = 10, sticky = "w")
         download_audio_button = tk.Button(
-            dl_frame, text="Get Audio", 
+            buttons_frame, text="Get Audio", 
             command = lambda: self.download("audio"))
-        download_audio_button.grid(row = 3, column = 1, padx = 10, sticky = "w")
+        download_audio_button.grid(row = 0, column = 1, padx = 10, sticky = "w")
+        # TODO
+        self.progressbar = ttk.Progressbar(buttons_frame, length = 200)
+        self.progressbar.grid(row = 0, column = 2, padx = 10, sticky = "w")
+    
 
-    # Scripts
+    # Script
     def save_download_folder(self) -> None:
-        folder = filedialog.askdirectory(title="select songs directory")
+        folder = filedialog.askdirectory(title="Select Download Folder")
         self.download_folder.set(folder)
     
     def save_video_url(self, url: tk.Entry) -> None:
         self.video_url.set(url.get())
         url.delete(0, tk.END)
+        self.get_video()
+
+    def get_video(self) -> None:
+        try:
+            self.video = apafy.new(self.video_url.get())
+            self.show_video_info()
+            self.show_download_buttons()
+        except ValueError:
+            messagebox.showerror(title = "Error", message = "Invalid URL, Try Again")
 
     def download(self, download_type) -> None:
-        video = apafy.new(self.video_url.get())
         stream = None
         if(download_type == "video"):
-            stream = video.getbest()
+            stream = self.video.getbest(
+                preftype="mp4", 
+                ftypestrict=False
+            )
         if(download_type == "audio"):
-            stream = video.getbestaudio()
-        print("Size is %s" % stream.get_filesize())
-        filename = stream.download(self.download_folder.get())  # starts download
-
+            stream = self.video.getbestaudio(
+                preftype="mp3", 
+                ftypestrict=False
+            )
+        # print("Size is %s" % stream.get_filesize())
+        stream.download(
+            filepath = self.download_folder.get(), 
+            quiet = False,
+            callback = self.download_status)  # starts download
     
-        
-
-
-
-
-
-
-# Video Info
-# title = tk.Label(self, text=f"Title: {video.title}")
-# title.grid(row = 1, column = 0)
-# rating = tk.Label(self, text=f"Rating: {video.rating}")
-# rating.grid(row = 1, column = 1)
-# author = tk.Label(self, text=f"Author: {video.author}")
-# author.grid(row = 1, column = 2)
-# viewcount = tk.Label(self, text=f"Views: {video.viewcount}")
-# viewcount.grid(row = 2, column = 0)
-# duration = tk.Label(self, text=f"Duration: {video.duration}")
-# duration.grid(row = 2, column = 1)
-# likes = tk.Label(self, text=f"Likes: {video.likes}")
-# likes.grid(row = 2, column = 2)
-# dislikes = tk.Label(self, text=f"Dislikes: {video.dislikes}")
-# dislikes.grid(row = 3, column = 0)
-# description = tk.Label(self, text=f"Description: {video.description}")
-# description.grid(row = 3, column = 1)
-# published = tk.Label(self, text=f"Published: {video.published}")
-# published.grid(row = 3, column = 2
-# image = tk.Label(self, text=f"Image: {video.bigthumb}")
-# image.grid(row = 4, column = 0)
+    # TODO
+    def download_status(self, total, recvd, ratio, rate, eta):
+        if(int(recvd) == 0): self.progressbar.configure(maximum=total)
+        else: self.progressbar.step(int(recvd) * 100)
